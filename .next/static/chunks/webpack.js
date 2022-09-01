b8ef83026be7b260d516a10a5ef4e38b4769e690
Luís Sobral <luissobral@MBP-de-Luis.home>
2022-09-01 18:26:26 +0100

    new version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f36557b96be606b2"; }
+/******/ 		__webpack_require__.h = function() { return "1900eb21fc53c7bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Index() {
   return (
     <>
       <Home/>
-      <div className="mx-8 mt-80 sm:mx-16 lg:mx-48">
+      <div className="mx-1 sm:mx-8 mt-80 md:mx-16 lg:mx-48">
         <About
           image="/ls.jpg"
           description="Hi! My name is Luís and computer science is something I enjoy to study since the first day I started. I am a computing science student at University of Minho specialized in distributed systems and aplications engeneering."
         />
       </div>
-      <div className="mx-8 mt-80 sm:mx-16 lg:mx-48">
+      <div className="mx-1 sm:mx-8 mt-80 md:mx-16 lg:mx-48">
         <Projects/>
       </div>
-      <div className="mx-8 mt-80 sm:mx-16 lg:mx-48">
+      <div className="mx-1 sm:mx-8 mt-80 md:mx-16 lg:mx-48">
         <GetTouch description="I hope you enjoyed meeting my website! I will keep working in more projects, for more content visit my github." link="https://github.com/luissobral4"/>
       </div>
     </>
