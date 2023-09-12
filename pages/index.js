5e74c25d15d7fb56885170874b0a97ab40db2e0d
Luís Sobral <luisfilipesobral@gmail.com>
2023-09-12 18:34:54 +0100

    erros

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index() {
       <div className="mx-1 sm:mx-8 mt-80 md:mx-16 lg:mx-48">
         <About
           image="/ls.jpg"
-          description="Hi! My name is Luís, I am now in my last year in the Computer Science Master Course at University of Minho and computer science is something I have enjoyed studying since the first day I started. I specialize in distributed systems and applications engineering."
+          description="Hi! My name is Luís, I'm now in my last year in the Computer Science Master Course at University of Minho and computer science has always been something i enjoyed, even since the first day i started. I'm specialized in distributed systems and applications engineering."
           description2="I am looking forward to continuing to grow professionally and personally. In 2021 I spent some time solving Haskell tasks on a Platform called Fiverr and learned to deal with client requests in a given delivery time. This year I joined a Just a Change International Camp In in Obidos, for 14 days I was with other Portuguese and International volunteers restoring houses in inhuman conditions and it was a really great experience that contributed to my personal growth."
           description3="In the future, I expect to face challenging experiences, keep working and knowing different people, and improve my Computer Science skills. "
         />
